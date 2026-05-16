3baa3909322e90427ea1646509d8f31f5ba3ab72
Шильников Даниил ИП-415 <laptch32@gmail.com>
2026-05-16 10:41:55 +0700

    lab6

--- vitest.config.ts
+++ vitest.config.ts
@@ -2,11 +2,8 @@ import { defineConfig } from 'vitest/config';
 
 export default defineConfig({
   test: {
+    globals: true,
     environment: 'node',
-    include: ['tests/**/*.ts'],
-    reporters: ['default', 'junit'],
-    outputFile: {
-      junit: './test-results/junit.html',
-    },
+    include: ['tests/**/*.test.ts'],
   },
 });
